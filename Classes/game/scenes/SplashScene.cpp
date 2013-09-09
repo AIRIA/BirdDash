275afd8dca4d56d4aec7b7dc1832549cff34e7bf
AIRIA <chaibingcheng0305@163.com>
2013-09-09 18:05:13 +0800

    add splash screen

---
+++ Classes/game/scenes/SplashScene.cpp
@@ -0,0 +1,24 @@
+﻿#include "SplashScene.h"
+#include "game/GameMain.h"
+
+bool SplashScene::init()
+{
+    if(!BaseScene::init())
+    {
+        return false;
+    }
+	CCSprite *splashImg = SPRITE(images/loading/splash.png);
+	splashImg->setPosition(VisibleRect::center());
+	addChild(splashImg);
+	SimpleAudioEngine::sharedEngine()->preloadBackgroundMusic("sounds/BGM/Main_bgm.mp3");
+	CCActionInterval *delayAct = CCDelayTime::create(3);
+	CCCallFunc *enterGameHandler = CCCallFunc::create(this,callfunc_selector(SplashScene::enterGame));
+	CCSequence *delaySeq = CCSequence::create(delayAct,enterGameHandler,NULL);
+	runAction(delaySeq);
+    return true;
+}
+
+void SplashScene::enterGame()
+{
+    CCDirector::sharedDirector()->replaceScene(GameMain::create());
+}
---
+++ Classes/game/scenes/SplashScene.h
@@ -0,0 +1,15 @@
+﻿#ifndef _SPLASHSCENE_H_
+#define _SPLASHSCENE_H_
+
+#include "common/CommonHeaders.h"
+#include "base/BaseScene.h"
+
+class SplashScene:public BaseScene
+{
+public:
+	void enterGame();
+	virtual bool init();
+	CREATE_FUNC(SplashScene);
+};
+
+#endif // !_SPLASHSCENE_H_
