db3928bfb1641efff3fe0c81608fea48ed51d63f
AIRIA <chaibingcheng0305@163.com>
2013-09-10 11:12:49 +0800

    masklayer 来屏蔽下方的显示对象接收事件

---
+++ Classes/base/MaskLayer.h
@@ -0,0 +1,15 @@
+﻿#ifndef _MASKLAYER_H_
+#define _MASKLAYER_H_
+
+#include "common/CommonHeaders.h"
+
+class MaskLayer:public CCLayerColor
+{
+public:
+    virtual bool init();
+    virtual void registerWithTouchDispatcher();
+	virtual bool ccTouchBegan(CCTouch *pTouch, CCEvent *pEvent);
+    CREATE_FUNC(MaskLayer);
+};
+
+#endif // !_MASKLAYER_H_
